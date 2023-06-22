e39e255f15250e88ef18832ca00068e2c45951ac
Your Name <you@example.com>
2023-06-22 19:10:16 +0530

    "changes" changes

--- app.js
+++ app.js
@@ -75,3 +75,15 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 app.use(express.static(path.resolve("./public")));
+const { v4 } = require('uuid');
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+module.exports = app;
