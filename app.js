056e97ec12eea7d57893dbddeae277e599936648
Your Name <you@example.com>
2023-06-23 22:21:01 +0530

    new changes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var bodyParser=require('body-parser');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors=require('cors');
@@ -11,8 +12,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
